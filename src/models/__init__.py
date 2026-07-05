40ef0a3be44a02aab5b714550e6761a8d27fa115
Urme <148221893+urmeo@users.noreply.github.com>
2026-07-05 06:18:53 +0600

    conditional __all__

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,10 +1,11 @@
 from .ml import get_classifier
 
+__all__ = ["get_classifier"]
+
 try:
     from .dl import CNN1DClassifier
 
     _TORCH_AVAILABLE = True
+    __all__.append("CNN1DClassifier")
 except ImportError:
     _TORCH_AVAILABLE = False
-
-__all__ = ["get_classifier", "CNN1DClassifier"]
